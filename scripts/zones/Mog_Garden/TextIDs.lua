40c75515075186b7b2d661601e56139ae92f69a6
x8jason8x <nalaonffxi@gmail.com>
2018-05-17 13:08:32 -0700

    update text ids

--- scripts/zones/Mog_Garden/TextIDs.lua
+++ scripts/zones/Mog_Garden/TextIDs.lua
@@ -1,17 +1,6 @@
 -- Variable TextID   Description text
 
 -- General Texts
-<<<<<<< HEAD
-ITEM_CANNOT_BE_OBTAINED  = 6380; -- You cannot obtain the item <item> come back again after sorting your inventory
-          ITEM_OBTAINED  = 6386; -- Obtained: <item>
-        ITEM_OBTAINEDX   = 6395; -- You obtain <number> <item>!
-           GIL_OBTAINED  = 6387; -- Obtained <number> gil
-       KEYITEM_OBTAINED  = 6389; -- Obtained key item: <keyitem>
-           KEYITEM_LOST  = 6390; -- Lost key item: <keyitem>
-    NOT_HAVE_ENOUGH_GIL  = 6393; -- You do not have enough gil
-MOGLOCKER_MESSAGE_OFFSET = 7510; -- Your particular paid period of Mog Locker patronage has been extended until the following time, kupo!
- FISHING_MESSAGE_OFFSET  = 7206; -- You can't fish here
-=======
 ITEM_CANNOT_BE_OBTAINED  = 6381; -- You cannot obtain the item <item> come back again after sorting your inventory
           ITEM_OBTAINED  = 6387; -- Obtained: <item>
         ITEM_OBTAINEDX   = 6396; -- You obtain <number> <item>!
@@ -20,7 +9,6 @@ ITEM_CANNOT_BE_OBTAINED  = 6381; -- You cannot obtain the item <item> come back 
     NOT_HAVE_ENOUGH_GIL  = 6394; -- You do not have enough gil
 MOGLOCKER_MESSAGE_OFFSET = 7511; -- Your particular paid period of Mog Locker patronage has been extended until the following time, kupo!
  FISHING_MESSAGE_OFFSET  = 7207; -- You can't fish here
->>>>>>> 99d42890e9a5003f512bd611fc3c5c6956e7d7bb
 
 -- Other dialog
 NOTHING_OUT_OF_ORDINARY  = 7585; -- There is nothing out of the ordinary here.

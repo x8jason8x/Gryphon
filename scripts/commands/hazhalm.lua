b671237879cb1b91965014ad2f59af5938096169
x8jason8x <nalaonffxi@gmail.com>
2018-05-18 04:10:26 -0700

    add hazhalm warp

---
+++ scripts/commands/hazhalm.lua
@@ -0,0 +1,14 @@
+---------------------------------------------------------------------------------------------------
+-- func: !hazhalm
+-- desc: allows players to check if odin or alex have spawned
+---------------------------------------------------------------------------------------------------
+
+cmdprops =
+{
+    permission = 0,
+    parameters = ""
+};
+
+function onTrigger(player, target)
+     player:setPos(476,-226,-20,138,78);
+end;

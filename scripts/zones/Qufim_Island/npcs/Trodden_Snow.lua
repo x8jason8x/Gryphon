6bd3417c037ec6717d7d944237d449354b30251e
x8jason8x <nalaonffxi@gmail.com>
2018-06-16 00:26:24 -0700

    update addon scripts

--- scripts/zones/Qufim_Island/npcs/Trodden_Snow.lua
+++ scripts/zones/Qufim_Island/npcs/Trodden_Snow.lua
@@ -1,136 +1,124 @@
 -----------------------------------
 -- Area: Qufim Island
---  NPC: Trodden Snow
--- Mission: ASA - THAT_WHICH_CURDLES_BLOOD
--- Mission: ASA - SUGAR_COATED_DIRECTIVE
--- @zone 126
--- !pos -19 -17 104
+-- NPC: Trodden Snow
+-- Missions: That Which Curdles Blood, Sugar Coated Directive
+-- !pos -19 -17 104 126
 -----------------------------------
-package.loaded["scripts/zones/Qufim_Island/TextIDs"] = nil;
+package.loaded["scripts/zones/Qufim_Island/TextIDs"] = nil
 -------------------------------------
-require("scripts/zones/Qufim_Island/TextIDs");
-require("scripts/globals/settings");
-require("scripts/globals/keyitems");
-require("scripts/globals/missions");
+require("scripts/zones/Qufim_Island/TextIDs")
+require("scripts/globals/settings")
+require("scripts/globals/keyitems")
+require("scripts/globals/missions")
 -----------------------------------
 
 function onTrade(player,npc,trade)
-    -- Trade Enfeebling Kit
-    if (player:getCurrentMission(ASA) == THAT_WHICH_CURDLES_BLOOD) then
-    local item = 0;
-    local asaStatus = player:getVar("ASA_Status");
 
-        -- TODO: Other Enfeebling Kits
+    if (player:getCurrentMission(ASA) == THAT_WHICH_CURDLES_BLOOD) then
+        local item = 0
+        local asaStatus = player:getVar("ASA_Status")
+     -- TODO: Other Enfeebling Kits
         if (asaStatus == 0) then
-            item = 2779;
+            item = 2779
         else
-            printf("Error: Unknown ASA Status Encountered <%u>", asaStatus);
-    end
+            printf("Error: Unknown ASA Status Encountered <%u>", asaStatus)
+        end
 
         if (trade:getItemCount() == 1 and trade:hasItemQty(item,1)) then
-            player:tradeComplete();
-    player:startEvent(44);
+            player:tradeComplete()
+            player:startEvent(44)
+        end
     end
-    end
-end;
+end
 
 function onTrigger(player,npc)
-    --ASA 4 CS: Triggers With At Least 3 Counterseals.
+
     if (player:getCurrentMission(ASA) == SUGAR_COATED_DIRECTIVE) then
-        local completedSeals = 0;
+        local completedSeals = 0
 
         if (player:hasKeyItem(dsp.ki.AMBER_COUNTERSEAL)) then
-            completedSeals = completedSeals + 1;
-        end;
+            completedSeals = completedSeals + 1
+        end
 
         if (player:hasKeyItem(dsp.ki.AZURE_COUNTERSEAL)) then
-            completedSeals = completedSeals + 1;
-        end;
+            completedSeals = completedSeals + 1
+        end
 
         if (player:hasKeyItem(dsp.ki.CERULEAN_COUNTERSEAL)) then
-            completedSeals = completedSeals + 1;
-        end;
+            completedSeals = completedSeals + 1
+        end
 
         if (player:hasKeyItem(dsp.ki.EMERALD_COUNTERSEAL)) then
-            completedSeals = completedSeals + 1;
-        end;
+            completedSeals = completedSeals + 1
+        end
 
         if (player:hasKeyItem(dsp.ki.SCARLET_COUNTERSEAL)) then
-            completedSeals = completedSeals + 1;
-        end;
+            completedSeals = completedSeals + 1
+        end
 
         if (player:hasKeyItem(dsp.ki.VIOLET_COUNTERSEAL)) then
-            completedSeals = completedSeals + 1;
-        end;
+            completedSeals = completedSeals + 1
+        end
 
         if (completedSeals >= 3) then
-            player:setVar("ASA_Status", completedSeals);
-        player:startEvent(45);
-        end;
+            player:setVar("ASA_Status", completedSeals)
+        player:startEvent(45)
+        end
     end
-end;
+end
 
 function onEventUpdate(player,csid,option)
-end;
+end
 
 function onEventFinish(player,csid,option)
 
-     if (csid==44) then
-            player:addKeyItem(dsp.ki.DOMINAS_SCARLET_SEAL);
-            player:messageSpecial(KEYITEM_OBTAINED,dsp.ki.DOMINAS_SCARLET_SEAL);
-            player:addKeyItem(dsp.ki.DOMINAS_CERULEAN_SEAL);
-            player:messageSpecial(KEYITEM_OBTAINED,dsp.ki.DOMINAS_CERULEAN_SEAL);
-            player:addKeyItem(dsp.ki.DOMINAS_EMERALD_SEAL);
-            player:messageSpecial(KEYITEM_OBTAINED,dsp.ki.DOMINAS_EMERALD_SEAL);
-            player:addKeyItem(dsp.ki.DOMINAS_AMBER_SEAL);
-            player:messageSpecial(KEYITEM_OBTAINED,dsp.ki.DOMINAS_AMBER_SEAL);
-            player:addKeyItem(dsp.ki.DOMINAS_VIOLET_SEAL);
-            player:messageSpecial(KEYITEM_OBTAINED,dsp.ki.DOMINAS_VIOLET_SEAL);
-            player:addKeyItem(dsp.ki.DOMINAS_AZURE_SEAL);
-            player:messageSpecial(KEYITEM_OBTAINED,dsp.ki.DOMINAS_AZURE_SEAL);
-
-            player:completeMission(ASA,THAT_WHICH_CURDLES_BLOOD);
-        player:addMission(ASA,SUGAR_COATED_DIRECTIVE);
-
-        player:setVar("ASA_Status",0);
-        player:setVar("ASA4_Amber","0");
-        player:setVar("ASA4_Azure","0");
-        player:setVar("ASA4_Cerulean","0");
-        player:setVar("ASA4_Emerald","0");
-        player:setVar("ASA4_Scarlet","0");
-        player:setVar("ASA4_Violet","0");
+    if (csid==44) then
+        player:addKeyItem(dsp.ki.DOMINAS_SCARLET_SEAL)
+        player:messageSpecial(KEYITEM_OBTAINED,dsp.ki.DOMINAS_SCARLET_SEAL)
+        player:addKeyItem(dsp.ki.DOMINAS_CERULEAN_SEAL)
+        player:messageSpecial(KEYITEM_OBTAINED,dsp.ki.DOMINAS_CERULEAN_SEAL)
+        player:addKeyItem(dsp.ki.DOMINAS_EMERALD_SEAL)
+        player:messageSpecial(KEYITEM_OBTAINED,dsp.ki.DOMINAS_EMERALD_SEAL)
+        player:addKeyItem(dsp.ki.DOMINAS_AMBER_SEAL)
+        player:messageSpecial(KEYITEM_OBTAINED,dsp.ki.DOMINAS_AMBER_SEAL)
+        player:addKeyItem(dsp.ki.DOMINAS_VIOLET_SEAL)
+        player:messageSpecial(KEYITEM_OBTAINED,dsp.ki.DOMINAS_VIOLET_SEAL)
+        player:addKeyItem(dsp.ki.DOMINAS_AZURE_SEAL);
+        player:messageSpecial(KEYITEM_OBTAINED,dsp.ki.DOMINAS_AZURE_SEAL)
+        player:completeMission(ASA,THAT_WHICH_CURDLES_BLOOD)
+        player:addMission(ASA,SUGAR_COATED_DIRECTIVE)
+        player:setVar("ASA_Status", 0)
+        player:setVar("ASA4_Amber", 0)
+        player:setVar("ASA4_Azure", 0)
+        player:setVar("ASA4_Cerulean", 0)
+        player:setVar("ASA4_Emerald", 0)
+        player:setVar("ASA4_Scarlet", 0)
+        player:setVar("ASA4_Violet", 0)
     elseif (csid==45) then
-            local completedSeals = player:getVar("ASA_Status");
-
-        -- Calculate Reward
+        local completedSeals = player:getVar("ASA_Status")
             if (completedSeals == 3) then
-                player:addGil(GIL_RATE*3000);
+                player:addGil(GIL_RATE*3000)
             elseif (completedSeals == 4) then
-                player:addGil(GIL_RATE*10000);
+                player:addGil(GIL_RATE*10000)
             elseif (completedSeals == 5) then
-                player:addGil(GIL_RATE*30000);
-               elseif (completedSeals == 6) then
-                player:addGil(GIL_RATE*50000);
+                player:addGil(GIL_RATE*30000)
+            elseif (completedSeals == 6) then
+                player:addGil(GIL_RATE*50000)
             end
-
-        -- Clean Up Remaining Key Items
-        player:delKeyItem(dsp.ki.DOMINAS_SCARLET_SEAL);
-            player:delKeyItem(dsp.ki.DOMINAS_CERULEAN_SEAL);
-            player:delKeyItem(dsp.ki.DOMINAS_EMERALD_SEAL);
-            player:delKeyItem(dsp.ki.DOMINAS_AMBER_SEAL);
-            player:delKeyItem(dsp.ki.DOMINAS_VIOLET_SEAL);
-            player:delKeyItem(dsp.ki.DOMINAS_AZURE_SEAL);
-
-            player:delKeyItem(dsp.ki.SCARLET_COUNTERSEAL);
-        player:delKeyItem(dsp.ki.CERULEAN_COUNTERSEAL);
-        player:delKeyItem(dsp.ki.EMERALD_COUNTERSEAL);
-        player:delKeyItem(dsp.ki.AMBER_COUNTERSEAL);
-        player:delKeyItem(dsp.ki.VIOLET_COUNTERSEAL);
-            player:delKeyItem(dsp.ki.AZURE_COUNTERSEAL);
-
-        -- Advance Mission
-            player:completeMission(ASA,SUGAR_COATED_DIRECTIVE);
-        player:addMission(ASA,ENEMY_OF_THE_EMPIRE_I);
-        player:setVar("ASA_Status",0);
+        player:delKeyItem(dsp.ki.DOMINAS_SCARLET_SEAL)
+        player:delKeyItem(dsp.ki.DOMINAS_CERULEAN_SEAL)
+        player:delKeyItem(dsp.ki.DOMINAS_EMERALD_SEAL)
+        player:delKeyItem(dsp.ki.DOMINAS_AMBER_SEAL)
+        player:delKeyItem(dsp.ki.DOMINAS_VIOLET_SEAL)
+        player:delKeyItem(dsp.ki.DOMINAS_AZURE_SEAL)
+        player:delKeyItem(dsp.ki.SCARLET_COUNTERSEAL)
+        player:delKeyItem(dsp.ki.CERULEAN_COUNTERSEAL)
+        player:delKeyItem(dsp.ki.EMERALD_COUNTERSEAL)
+        player:delKeyItem(dsp.ki.AMBER_COUNTERSEAL)
+        player:delKeyItem(dsp.ki.VIOLET_COUNTERSEAL)
+        player:delKeyItem(dsp.ki.AZURE_COUNTERSEAL)
+        player:completeMission(ASA,SUGAR_COATED_DIRECTIVE)
+        player:addMission(ASA,ENEMY_OF_THE_EMPIRE_I)
+        player:setVar("ASA_Status",0)
     end
-end;
+end
--- scripts/zones/Qufim_Island/npcs/qm3.lua
+++ scripts/zones/Qufim_Island/npcs/qm3.lua
@@ -2,56 +2,55 @@
 -- Area: Qufim Island
 --  NPC: ??? (qm3)
 -- Mission: ACP - The Echo Awakens
--- @zone 126
--- !pos -120.342 -19.471 306.661
+-- !pos -120 -19 306 126
 -----------------------------------
-package.loaded["scripts/zones/Qufim_Island/TextIDs"] = nil;
+package.loaded["scripts/zones/Qufim_Island/TextIDs"] = nil
 -------------------------------------
-require("scripts/zones/Qufim_Island/TextIDs");
-require("scripts/globals/settings");
-require("scripts/globals/keyitems");
-require("scripts/globals/missions");
+require("scripts/zones/Qufim_Island/TextIDs")
+require("scripts/globals/settings")
+require("scripts/globals/keyitems")
+require("scripts/globals/missions")
 -----------------------------------
 
 function onTrade(player,npc,trade)
-    -- Trade Seedspall's Lux, Luna, Astrum
+
     if (player:getCurrentMission(ACP) == THE_ECHO_AWAKENS and trade:getItemCount() == 3
     and trade:hasItemQty(2740,1) and trade:hasItemQty(2741,1) and trade:hasItemQty(2742,1)) then
-        player:tradeComplete();
-        player:startEvent(31);
+        player:tradeComplete()
+        player:startEvent(31)
     end
-end;
+end
 
 function onTrigger(player,npc)
-    
-	local ACPm = player:getCurrentMission(ACP);
-    local now = tonumber(os.date("%j"));
+
+    local ACPm = player:getCurrentMission(ACP)
+    local now = tonumber(os.date("%j"))
     local SR = player:hasKeyItem(dsp.ki.SEEDSPALL_ROSEUM)
     local SC = player:hasKeyItem(dsp.ki.SEEDSPALL_CAERULUM)
     local SV = player:hasKeyItem(dsp.ki.SEEDSPALL_VIRIDIS)
-    local AmberKey = player:hasKeyItem(dsp.ki.AMBER_KEY);
-    local LastAmber = player:getVar("LastAmberKey"); -- When last Amber key was obtained
-    local LastViridian = player:getVar("LastViridianKey"); -- When last Viridian key was obtained
+    local AmberKey = player:hasKeyItem(dsp.ki.AMBER_KEY)
+    local LastAmber = player:getVar("LastAmberKey")
+    local LastViridian = player:getVar("LastViridianKey")
 
     if (ENABLE_ACP == 1 and player:hasKeyItem(dsp.ki.AMBER_KEY) == false) then
         if (ACPm == GATHERER_OF_LIGHT_I and SR and SC and SV and now ~= LastViridian) then
-            player:startEvent(32);
+            player:startEvent(32)
         elseif (ACPm == GATHERER_OF_LIGHT_II and player:getVar("SEED_MANDY") == 0) then
-            -- Spawn Seed mandragora's
-            player:setVar("SEED_MANDY",1); -- This will need moved into Seed mandies onDeath script later.
+            -- spawn Seed Mandragoras
+            player:setVar("SEED_MANDY",1) -- This will need moved into Seed mandies onDeath script later.
             player:PrintToPlayer( "Confrontation Battles are not working yet, click again to continue ACP missions." );
             -- dsp.effect.CONFRONTATION for 30 min
         elseif (ACPm == GATHERER_OF_LIGHT_II and player:getVar("SEED_MANDY") == 1) then -- change SEED_MANDY var number later when battle actually works (intended purpose is to track number of slain mandies).
-            player:setVar("SEED_MANDY",0);
-            player:startEvent(34);
+            player:setVar("SEED_MANDY",0)
+            player:startEvent(34)
         -- elseif (ACPm >= THOSE_WHO_LURK_IN_SHADOWS_I and AmberKey == false and now ~= LastAmber and now ~= LastViridian and SR and SC and SV and player:getVar("SEED_MANDY") == 0) then
             -- This is for repeats to get amber keys.
-            -- Spawn Seed mandragora's with dsp.effect.CONFRONTATION for 30 min
+            -- spawn Seed Mandragoras with dsp.effect.CONFRONTATION for 30 min
         -- elseif (SR and SC and SV and ACPm >= THOSE_WHO_LURK_IN_SHADOWS_I and player:getVar("SEED_MANDY") == 1) then
-            -- player:addKeyItem(dsp.ki.AMBER_KEY);
-            -- player:setVar("LastAmberKey", os.date("%j"));
-            -- player:messageSpecial(KEYITEM_OBTAINED,dsp.ki.AMBER_KEY);
-            -- player:setVar("SEED_MANDY",0);
+            -- player:addKeyItem(dsp.ki.AMBER_KEY)
+            -- player:setVar("LastAmberKey", os.date("%j"))
+            -- player:messageSpecial(KEYITEM_OBTAINED,dsp.ki.AMBER_KEY)
+            -- player:setVar("SEED_MANDY",0)
             -- player:delKeyItem(dsp.ki.SEEDSPALL_ROSEUM)
             -- player:delKeyItem(dsp.ki.SEEDSPALL_CAERULUM)
             -- player:delKeyItem(dsp.ki.SEEDSPALL_VIRIDIS)
@@ -61,23 +60,24 @@ function onTrigger(player,npc)
     else
         -- Todo: find retail message (if any) and text its ID
     end
-end;
+end
 
 function onEventUpdate(player,csid,option)
-end;
+end
 
 function onEventFinish(player,csid,option)
+
     if (csid == 31) then
-        player:completeMission(ACP,THE_ECHO_AWAKENS);
-        player:addMission(ACP,GATHERER_OF_LIGHT_I);
+        player:completeMission(ACP,THE_ECHO_AWAKENS)
+        player:addMission(ACP,GATHERER_OF_LIGHT_I)
     elseif (csid == 32) then
-        player:completeMission(ACP,GATHERER_OF_LIGHT_I);
-        player:addMission(ACP,GATHERER_OF_LIGHT_II);
+        player:completeMission(ACP,GATHERER_OF_LIGHT_I)
+        player:addMission(ACP,GATHERER_OF_LIGHT_II)
         player:delKeyItem(dsp.ki.SEEDSPALL_ROSEUM)
         player:delKeyItem(dsp.ki.SEEDSPALL_CAERULUM)
         player:delKeyItem(dsp.ki.SEEDSPALL_VIRIDIS)
     elseif (csid == 34) then
-        player:completeMission(ACP,GATHERER_OF_LIGHT_II);
-        player:addMission(ACP,THOSE_WHO_LURK_IN_SHADOWS_I);
+        player:completeMission(ACP,GATHERER_OF_LIGHT_II)
+        player:addMission(ACP,THOSE_WHO_LURK_IN_SHADOWS_I)
     end
-end;
+end

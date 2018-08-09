40d20b74e7ac7981ea431c27a9602f5e6eb3c37e
x8jason8x <nalaonffxi@gmail.com>
2018-08-09 11:14:13 -0700

    commit for update

--- scripts/zones/Lower_Delkfutts_Tower/npcs/Seed_Fragment.lua
+++ scripts/zones/Lower_Delkfutts_Tower/npcs/Seed_Fragment.lua
@@ -10,8 +10,6 @@ require("scripts/zones/Lower_Delkfutts_Tower/TextIDs");
 
 function onTrigger(player,npc)
 
-    if player:getCurrentMission(ACP) == BORN_OF_HER_NIGHTMARES then
-        player:PrintToPlayer("Level restriction and key items are not yet implemented.")
-        player:PrintToPlayer("Check the Seed Fragment in Stellar Fulcrum to proceed.")
-    end
+    player:PrintToPlayer("For ACP mission, level restriction and key items are not yet implemented.")
+    player:PrintToPlayer("Check the Seed Fragment in Stellar Fulcrum to proceed.")
 end
